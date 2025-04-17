cb8b67b2775c1651fdb0191789caa52da109d934
Tom Svensson <tom_svensson@outlook.com>
2025-04-17 11:05:26 +0200

    updated .gitignore

--- start-project.cmd
+++ start-project.cmd
@@ -2,7 +2,7 @@
 echo Starting the Blogg App...
 
 :: Start frontend (Vue.js)
-start cmd /k "cd client && npm install && npm run dev"
+start cmd /k "cd client && npm install && npm run serve"
 
 :: Start backend (ASP.NET Web API)
 start cmd /k "cd server && dotnet restore && dotnet run"
