9ec66a62cf2660b55cb9670be9199f0e96385875
Tom Svensson <tom_svensson@outlook.com>
2025-04-17 10:51:16 +0200

    la till start-project.cmd

---
+++ start-project.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo Starting the Blogg App...
+
+:: Start frontend (Vue.js)
+start cmd /k "cd client && npm install && npm run dev"
+
+:: Start backend (ASP.NET Web API)
+start cmd /k "cd server && dotnet restore && dotnet run"
